68b7c5d03af8a562a42c27df6958d1f4d3b2d75d
fxalban8 <francisco.alban@epn.edu.ec>
2020-05-25 18:16:10 -0500

    It's been added a command line that show the number of elements of a list

--- Creating_and_Manipulating Lists.py
+++ Creating_and_Manipulating Lists.py
@@ -2,3 +2,5 @@
 program_languages=["Python","Java","C++","C#"]
 #We can use the command TYPE() to verify that program_languages is a list
 print(type(program_languages))
+#To know how many elements have been added to the list we can use the len() command
+print("The number of elements of the list is: "+str(len(program_languages)))
