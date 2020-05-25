376f669ea6d14598f2af14f2c45f55818c82253e
fxalban8 <francisco.alban@epn.edu.ec>
2020-05-25 18:14:07 -0500

    It's been used the command type to verify that the variable created is a list

--- Creating_and_Manipulating Lists.py
+++ Creating_and_Manipulating Lists.py
@@ -1,2 +1,4 @@
 #A list is created by using brackets [] and separating each element with commas
 program_languages=["Python","Java","C++","C#"]
+#We can use the command TYPE() to verify that program_languages is a list
+print(type(program_languages))
