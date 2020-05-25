db65eaf29c9699a8b60d58eb1e47f015f16e7941
fxalban8 <francisco.alban@epn.edu.ec>
2020-05-25 18:17:45 -0500

    The elements of the list have been printed

--- Creating_and_Manipulating Lists.py
+++ Creating_and_Manipulating Lists.py
@@ -4,3 +4,5 @@ program_languages=["Python","Java","C++","C#"]
 print(type(program_languages))
 #To know how many elements have been added to the list we can use the len() command
 print("The number of elements of the list is: "+str(len(program_languages)))
+#If we prefere to show all the list, we can do it by executing a print command
+print(program_languages)
